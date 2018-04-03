9d32651d624598c4829aed3c8aafee5d9f136718
Shawn T. Brown <stbrown@msin.ca>
2018-04-03 08:31:52 -0400

    Added Bourreau logic to provisioning

--- provision/setup.sh
+++ provision/setup.sh
@@ -75,6 +75,9 @@ cd `bundle show sys-proctable`; rake install >> $logFile
 cd $HOME/cbrain/BrainPortal/config/initializers
 sed 's/simple_name/CBRAINVagrant/g' config_portal.rb.TEMPLATE > config_portal.rb
 
+cd $HOME/cbrain/Bourreau/config/initializers
+sed 's/simple_name/LocalBourreau/g' config_bourreau.rb.TEMPLATE > config_bourreau.rb
+
 ### Initialize Database Schema
 
 echo "Database Schema Initialization" >> $logFile
@@ -123,18 +126,37 @@ rm -rf $HOME/singTemp
 #### Set up the BrainPortal Cache and timezone
 cd $HOME/cbrain/BrainPortal
 
-echo $HOME | xargs -I {} echo 'p=RemoteResource.first; p.dp_cache_dir="{}/BorCache"; p.save' | rails c >> $logFile
+echo $HOME | xargs -I {} echo 'p=RemoteResource.first; p.dp_cache_dir="{}/BPCache"; p.save' | rails c >> $logFile
 timezone=`timedatectl status | grep 'Time zone' | awk '{print $3}'`
-echo 'p=User.first; p.time_zone = "$timezone"; p.save' | rails c >> $logFile
+echo $timezone | xargs -I {} echo 'p=User.first; p.time_zone = "{}"; p.save' | rails c >> $logFile
 
 ### Setup local DataProvider
 
-echo $HOME | xargs -I {} echo 'DataProvider.create :id => 2, :name => "LocalDP", :type => "FlatDirLocalDataProvider", :remote_dir => "{}/FlatLocalDP", :time_zone => $timezone, :description => "Automatically Generated Local Data Provider", :user_id => 1, :group_id => 1, :online => 1' | rails c >> $logFile
+echo $HOME | xargs -I {} echo 'DataProvider.create :id => 2, :name => "LocalDP", :type => "FlatDirLocalDataProvider", :remote_dir => "{}/FlatLocalDP", :description => "Automatically Generated Local Data Provider", :user_id => 1, :group_id => 1, :online => 1' | rails c >> $logFile
+echo $timezone | xargs -I {} echo 'd=DataProvider.where("id=2").first; d.time_zone = "{}"; d.save' | rails c >> $logFile
 
 cat ~/.ssh/id_cbrain_portal.pub >> ~/.ssh/authorized_keys
-echo $HOME | xargs -I {} echo 'DataProvider.create :id => 3, :name => "SshDP", :type => "FlatDirSshDataProvider", :remote_dir => "{}/SshDP", :time_zone => $timezone, :description => "Automatically Generated SSH Data Provider", :user_id => 1, :group_id => 1, :online => 1' | rails c >> $logFile
+echo $HOME | xargs -I {} echo 'DataProvider.create :id => 3, :name => "SshDP", :type => "FlatDirSshDataProvider", :remote_dir => "{}/SshDP", :description => "Automatically Generated SSH Data Provider", :user_id => 1, :group_id => 1, :online => 1' | rails c >> $logFile
+echo $timezone | xargs -I {} echo 'd=DataProvider.where("id=3").first; d.time_zone = "{}"; d.save' | rails c >> $logFile
 echo $USER | xargs -I {} echo 'd=DataProvider.where("id=3").first; d.remote_user = "{}"; d.remote_host = "localhost"; d.remote_port = 22; d.save' | rails c >> $logFile
 
-echo $HOME | xargs -I {} echo 'DataProvider.create :id => 4, :name => "CbrainDP", :type => "EnCbrainSmartDataProvider", :remote_dir => "{}/CBLocalDP", :time_zone => $timezone, :description => "Automatically Generated Smart Data Provider", :user_id => 1, :group_id => 1, :online => 1' | rails c >> $logFile
+echo $HOME | xargs -I {} echo 'DataProvider.create :id => 4, :name => "CbrainDP", :type => "EnCbrainSmartDataProvider", :remote_dir => "{}/CBLocalDP", :description => "Automatically Generated Smart Data Provider", :user_id => 1, :group_id => 1, :online => 1' | rails c >> $logFile
 echo $USER | xargs -I {} echo 'd=DataProvider.where("id=4").first; d.remote_user = "{}"; d.remote_host = "localhost"; d.remote_port = 22; d.save' | rails c >> $logFile
+echo $timezone | xargs -I {} echo 'd=DataProvider.where("id=4").first; d.time_zone = "{}"; d.save' | rails c >> $logFile
 
+#### Set up the local Bourreaux
+
+# Make sure you can see bundle at login
+cd $HOME
+echo 'export PATH="$PATH:$HOME/.rvm/bin"' > .tmpbashrc
+echo  '[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"' >> .tmpbashrc 
+cat .bashrc >> .tmpbashrc
+cp .bashrc .bashrc.old
+mv .tmpbashrc .bashrc
+
+# Create the Bourreau
+cd $HOME/cbrain/BrainPortal
+
+echo $HOME | xargs -I {} echo 'RemoteResource.create :id => 2, :name => "LocalBourreau", :type => "Bourreau", :user_id => 1, :group_id => 1, :online => 1, :description => "Automatically Generated Local Bourreau", :ssh_control_host => "localhost", :ssh_control_port => 22, :ssh_control_rails_dir => "{}/cbrain/Bourreau", :tunnel_mysql_port => 3333, :tunnel_actres_port => 3334, :dp_cache_dir => "{}/BorCache", :workers_instances=> 3, :workers_chk_time => 5, :workers_log_to => "combined", :workers_verbose => 1, :read_only => 0, :portal_locked => 0, :cache_trust_expire => 2592000, :cms_class => "ScirUnix", :cms_shared_dir => "{}/BorWork", :docker_executable_name => "docker", :singularity_executable_name => "singularity"' | rails c >> $logFile
+echo $USER | xargs -I {} echo 'p=RemoteResource.where("id=2").first; p.ssh_control_user = "{}"; p.save' | rails c >> $logFile
+echo $timezone | xargs -I {} echo 'p=RemoteResource.where("id=2").first; p.time_zone = "{}"; p.save' | rails c >> $logFile 
